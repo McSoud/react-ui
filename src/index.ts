bb31c89f2b08e76286ee63cbcf865bda53d42a52
mcsoud <fmcsoud@gmail.com>
2025-08-05 18:50:16 +0300

    Changed svgs import, added loading-spin keyframes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import Main from "./components/Main";
 import Modal from "./components/Modal";
 import { toggleModal } from "./functions";
 import useLaravelQuery, { UI_OPTIONS } from "./components/Display";
-import * as svgs from "./svg";
 import "./assets/css/index.css";
 
 interface ModalsRegistration {}
@@ -11,4 +10,5 @@ export type ModalIds = ModalsRegistration extends { modals: infer T }
   ? T
   : "example";
 
-export { Main, Modal, useLaravelQuery, UI_OPTIONS, toggleModal, svgs };
+export { Main, Modal, useLaravelQuery, UI_OPTIONS, toggleModal };
+export * from "./svg";
