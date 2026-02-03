49445dee825f24b3769a08de1992dfb49b8c315d
mcsoud <fmcsoud@gmail.com>
2026-02-03 18:31:51 +0200

    Added snapchat and trash

--- src/svg.ts
+++ src/svg.ts
@@ -1,5 +1,6 @@
 import SvgLoading from "./assets/svg/icons/Loading";
 import SvgClose from "./assets/svg/icons/Close";
+import SvgTrash from "./assets/svg/icons/Trash";
 import SvgMail from "./assets/svg/socials/Mail";
 import SvgLocation from "./assets/svg/socials/Location";
 import SvgPhone from "./assets/svg/socials/Phone";
@@ -9,6 +10,7 @@ import SvgInstagram from "./assets/svg/socials/Instagram";
 import SvgTikTok from "./assets/svg/socials/TikTok";
 import SvgWhatsapp from "./assets/svg/socials/Whatsapp";
 import SvgYoutube from "./assets/svg/socials/Youtube";
+import SvgSnapchat from "./assets/svg/socials/Snapchat";
 import SvgApple from "./assets/svg/socials/Apple";
 import SvgGooglePlay from "./assets/svg/socials/GooglePlay";
 import SvgLink from "./assets/svg/icons/Link";
@@ -18,6 +20,7 @@ import SvgEyeOff from "./assets/svg/icons/EyeOff";
 export {
   SvgLoading,
   SvgClose,
+  SvgTrash,
   SvgMail,
   SvgLocation,
   SvgPhone,
@@ -27,6 +30,7 @@ export {
   SvgTikTok,
   SvgWhatsapp,
   SvgYoutube,
+  SvgSnapchat,
   SvgApple,
   SvgGooglePlay,
   SvgLink,
